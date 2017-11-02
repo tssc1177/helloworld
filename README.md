5706c31bf4e6f807df4e6270a16e297fe7f9b06e
zhaoshuai <zyc613@qq.com>
2017-11-02 10:24:52 +0800

    commit

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # hello-world
 It is zhaoshuai's project in github.
-
-
-this is 20171026
-please call me ares.
--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,5 @@
 # -*- coding: utf-8 -*-
 # Author:Zhao Shuai
 """***"""
-print("This is github.com in 20171026.")
 print("hello world")
-
-print("aaaaaa")
-
 
